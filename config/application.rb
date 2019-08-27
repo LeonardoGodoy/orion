b44c2e9d3efbb2ee5c5ffa29b470a6f19062c2dc
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-08-27 19:22:51 -0300

    Remove i18n default locale

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Orion
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.i18n.default_locale = :'pt-BR'
+    # config.i18n.default_locale = :'pt-BR'
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
