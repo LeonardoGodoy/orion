ad99a1128baa3010138585dee955f0c6b8f12d3c
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-09-08 10:35:34 -0300

    Add subscriptions model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :institution, optional: true
   belongs_to :course, optional: true
   belongs_to :discipline, optional: true
+
+  has_many :subscriptions
+  has_many :users, through: :subscriptions
 end
