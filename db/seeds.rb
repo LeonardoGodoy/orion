68470220c4441859d5ffe4d0e5d230426644279d
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-09-18 21:54:31 -0300

    Add basic seeds to development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# To restore development enviroment run:
+# rails db:drop db:create db:migrate db:seed
+
+if Rails.env.development?
+  User.create(name: 'Admin', email: 'admin@admin.com', password: '123123')
+  User.create(name: 'User', email: 'user@user.com', password: '123123')
+
+  20.times do
+    name = Faker::Name.name
+    email = Faker::Internet.safe_email(name: name)
+
+    User.create(name: name, email: email, password: '123123')
+  end
+
+  50.times do
+    institution = Institution.create(name: Faker::Educator.university)
+    course = Course.create(name: Faker::Educator.course_name)
+    discipline = Discipline.create(name: Faker::Educator.subject)
+    group = Group.create(institution: institution, course: course, discipline: discipline)
+  end
+end
