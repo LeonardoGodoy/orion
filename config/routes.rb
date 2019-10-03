ca384ac0e74eba0fcc66dedfc9d515fe4daa1df5
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-10-02 22:22:39 -0300

    Add ban rout

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,6 @@ Rails.application.routes.draw do
     resources :institutions, except: [:new, :edit, :destroy]
     resources :groups, except: [:new, :edit, :destroy]
     resources :subscriptions, only: [:index, :create, :update, :destroy]
+    resources :managers, only: [:create, :destroy]
   end
 end
