f9f775ec3d838599bfc52578d7dd11980ed4e3e2
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-10-10 19:51:20 -0300

    Add group description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_155529) do
+ActiveRecord::Schema.define(version: 2019_10_10_223948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_155529) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["course_id"], name: "index_groups_on_course_id"
     t.index ["discipline_id"], name: "index_groups_on_discipline_id"
     t.index ["institution_id"], name: "index_groups_on_institution_id"
