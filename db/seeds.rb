309e3f4d6f696579d35dc5e6f4915a1ad1dd88f3
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-11-05 09:47:14 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,87 +1,64 @@
+require_relative './seeds/load_institutions.rb'
+require_relative './seeds/load_courses.rb'
+require_relative './seeds/load_students.rb'
+require_relative './seeds/seeds_helper.rb'
 # To restore development enviroment run:
 # rails db:drop db:create db:migrate db:seed
 
+puts 'Loading institutions...'
+Seeds::LoadInstitutions.new.perform
+puts 'Loading courses...'
+Seeds::LoadCourses.new.perform
+puts 'Loading admin...'
+Admin.create(name: 'Admin', email: 'admin@admin.com', password: '123123')
+puts 'Loading students...'
+@members ||= Seeds::LoadStudents.new.perform
+
 if Rails.env.development?
-  admin   = Admin.create(name: 'Admin', email: 'admin@admin.com', password: '123123')
-  student = Student.create(name: 'User', email: 'user@user.com', password: '123123')
+  puts 'Generating random students...'
+  15.times { Seeds::SeedsHelper.generate_student }
 
-  50.times do
-    name  = Faker::Name.name
-    email = Faker::Internet.safe_email(name: name)
-    Student.create(name: name, email: email, password: '123123')
+  puts 'Generating random groups...'
+  Discipline.all.each do |discipline|
+    Seeds::SeedsHelper.generate_group(Student.all.sample, discipline)
   end
 
-  def generate_group(user)
-    institution = Institution.create(name: Faker::Educator.university)
-    course      = Course.create(name: Faker::Educator.course_name)
-    discipline  = Discipline.create(name: Faker::Educator.subject)
+  puts 'Subscribing members...'
+  ['DS660', 'DS661', 'DS260', 'DS960', 'DS802', 'DS803', 'DS804', 'DS871', 'DS875'].each do |code|
+    discipline = Discipline.find_by('name ilike :code', code: "#{code}%")
+    group = discipline.groups.first
 
-    group_params = {
-      institution_id: institution.id,
-      course_id: course.id,
-      discipline_id: discipline.id,
-      name: Faker::Team.name,
-      description: '...'
-    }
+    @members.each do |member|
+      form = SubscribeForm.new(group_id: group.id, user_id: member.id)
+      form.perform
+      rand(1..3).times { Seeds::SeedsHelper.generate_performance(member, group) }
 
-    form = CreateGroupForm.new(group_params)
-    form.user = user
-    form.save
-  end
-
-  40.times { generate_group(Student.all.sample) }
-  4.times { generate_group(student) }
-  6.times { SubscribeForm.new(group_id: Group.all.sample.id, user_id: student.id).perform }
-
-  student.groups.each do |group|
-    rand(2..5).times { |i| Performance.create(user_id: student.id, discipline_id: group.discipline.id, description: "Nota #{i + 1}", value: rand(33..100), max_value: 100) }
-    rand(4..9).times { SubscribeForm.new(group_id: group, user_id: Student.all.sample.id).perform }
-    rand(8..15).times { SubscribeForm.new(group_id: group.id, user_id: Student.where.not(id: student.id).sample.id).perform }
-
-    rand(1..5).times do |i|
-      data = {
-        title: "Título #{i + 1}",
-        content: "Conteúdo #{i + 1}",
-        date: Time.now.next_day(rand(0..15)),
-        user_id: Student.joins(:subscriptions).where(subscriptions: { group_id: group.id }).sample.id,
-        group_id: group.id
-      }
-
-      CreateEventForm.new(data).perform
+      CreateManagerForm.new(subscription_id: form.subscription.id, user_id: group.subscriptions.managers.first.user_id).perform
     end
 
+    rand(3..5).times { SubscribeForm.new(group_id: group.id, user_id: Student.all.sample.id).perform  }
+  end
 
-    files = Dir.new(Rails.root.join('app', 'assets', 'images')).children.select { |name| name.ends_with?('png') }
+  puts 'Generating events...'
+  Group.all.each do |group|
+    rand(2..5).times { Seeds::SeedsHelper.generate_event(group.users.sample, group) }
+  end
 
-    rand(2..12).times do |i|
-      data = {
-        title: "Post #{i + 1}",
-        content: "Conteúdo #{i + 1}",
-        user_id: Student.joins(:subscriptions).where(subscriptions: { group_id: group.id }).sample.id,
-        group_id: group.id
-      }
+  puts 'Generating posts...'
+  Group.all.each do |group|
+    students = Student.joins(:subscriptions)
+                      .where(subscriptions: { group_id: group.id })
+    rand(3..6).times do
+      Seeds::SeedsHelper.generate_post(students.sample, group)
+    end
+  end
 
-      form = CreatePostForm.new(data)
-      if form.perform
-        rand(0..2).times do
-          file = files.sample
-          path = Rails.root.join('app', 'assets', 'images', file)
-          form.post
-              .files
-              .attach(io: File.open(path), filename: file)
-        end
+  puts 'Generating classifications'
+  Post.all.each do |post|
+    students = post.group.users
 
-        rand(1..10).times do
-          user_id = Student.joins(:subscriptions)
-                           .where(subscriptions: { group_id: group.id }).sample.id
-          post_id = form.post.id
-          like = rand(0..3) > 1
-
-          CreatePostClassificationForm.new(post_id: post_id, user_id: user_id, like: like).perform
-        end
-      else
-        puts form.post.errors.messages
-      end
+    rand(2..6).times do
+      Seeds::SeedsHelper.generate_classification(students.sample, post)
     end
   end
 end
