210d6ac6e82a4b65b0d899e2cdb698cb73c717a0
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-11-12 20:42:37 -0300

    Remove event hour from serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,14 +1,6 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :date, :hour, :created_at, :updated_at
+  attributes :id, :title, :content, :date, :created_at, :updated_at
 
   belongs_to :user, key: :student
   belongs_to :group
-
-  def date
-    I18n.l(object.date, format: '%Y-%m-%d')
-  end
-
-  def hour
-    I18n.l(object.date, format: '%H-%M')
-  end
 end
