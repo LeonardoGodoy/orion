b0e1f7bd1718d2d3ddd1de8062cc667a4a71a393
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2019-11-17 20:18:00 -0300

    Add user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :trackable,
          :recoverable, :rememberable, :validatable
 
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   has_many :groups, through: :subscriptions
-  has_many :performances
+  has_many :performances, dependent: :destroy
+  has_many :events, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :classifications, class_name: 'PostClassification', dependent: :destroy
 end
